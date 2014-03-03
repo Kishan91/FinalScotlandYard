c7325d4ae1bf5e5ce6c83a49400ce3117cd6fa31
I Malik <im13557@my.bristol.ac.uk>
2014-03-03 21:31:57 +0000

    gui working

--- ScotlandYard/src/Map2.java
+++ ScotlandYard/src/Map2.java
@@ -12,26 +12,24 @@ public class Map2 extends JFrame implements Runnable {
 		map.run();
 	}
 
-	public void run()
+	private void run()
 	{
-		/*Dimension screenSize = Toolkit.getDefaultToolkit().getScreenSize();
-		double width = screenSize.getWidth();
-		double height = screenSize.getHeight();
-		width = width*0.8;
-		height = height*0.8;*/
-		printf("iman is watching you terminal");
 		JFrame window = new JFrame("Scotland Yard");
 		setTitle("Scotland Yard");
-		Dimension a = Toolkit.getDefaultToolkit().getScreenSize();
-		printf(a.getWidth());
-		printf(a.getHeight());
-		window.setExtendedState(JFrame.MAXIMIZED_BOTH);
-		window.setResizable(true);
+		Dimension screenSize = Toolkit.getDefaultToolkit().getScreenSize();
+		Insets scnMax = Toolkit.getDefaultToolkit().getScreenInsets(getGraphicsConfiguration());
+		int bottom = scnMax.bottom;
+		int left = scnMax.left;
+		int right = scnMax.right;
+		int top = scnMax.top;
+		int screenWidth = (int) screenSize.getWidth() - left - right - getWidth();
+		int screenHeight = (int) screenSize.getHeight() - bottom - top - getHeight();
+		Dimension a = new Dimension(screenWidth,screenHeight);
+		window.setPreferredSize(a);
 		window.setVisible(true);
+		window.setResizable(false);
 		window.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
 		window.pack();
-
-
 		URL london = this.getClass().getResource("map.jpg");
 		BufferedImage image = null;
 		try{
@@ -41,29 +39,12 @@ public class Map2 extends JFrame implements Runnable {
 		}
 		int width = (int) window.getWidth();
 		int height = (int) window.getHeight();
-		/*printf("\n");
-		printf(width);
-		printf(height);*/
 		BufferedImage buffered = image;
 		int buffHeight= (int) buffered.getHeight();
 		int buffWidth = (int) buffered.getWidth();
-		printf(buffWidth);
-
-		printf(buffHeight);
-		/*int buffHeight = 1018;
-		int buffWidth = 809;
-		printf("\n");*/
 		Dimension buffDimension = new Dimension(buffWidth,buffHeight);
 		Dimension desDimension =  new Dimension(width,height);
 		desDimension = aspectRatio(buffDimension,desDimension);
-		printf("\n");
-		printf (desDimension.getWidth());
-		printf("\n");
-		printf (desDimension.getHeight());
-
-
-		/*printf(desDimension.getWidth());
-		printf(desDimension.getHeight());*/
 		BufferedImage resized = resize(buffered, desDimension);
 		JLabel background  = new JLabel(new ImageIcon(resized));
 		window.setResizable(false);
@@ -76,30 +57,14 @@ public class Map2 extends JFrame implements Runnable {
 		System.out.print(o);
 	}
 
-	/*private Dimension aspectRatio (Dimension image, Dimension requiredImage)
+	private Dimension aspectRatio(Dimension imgSize, Dimension boundary) 
 	{
-		int original_width = (int) image.getWidth();
-		int original_height = (int) image.getHeight();
-		int designer_width = (int) requiredImage.getWidth();
-		int designer_height = (int) requiredImage.getHeight();
-		int original_ratio = (int) original_width / original_height;
-		int designer_ratio = (int) designer_width / designer_height;
-		if 	(original_ratio > designer_ratio)
-		{
-    		designer_height =  (int) designer_width / original_ratio;
-		}
-		else designer_width =  (int) designer_height * original_ratio;
-		image = new Dimension(designer_width, designer_height);
-		return image;*/
-		private Dimension aspectRatio(Dimension imgSize, Dimension boundary) {
-
     int original_width = imgSize.width;
     int original_height = imgSize.height;
     int bound_width = boundary.width;
     int bound_height = boundary.height;
     int new_width = original_width;
     int new_height = original_height;
-
     // first check if we need to scale width
     if (original_width > bound_width) {
         //scale width to fit
@@ -107,7 +72,6 @@ public class Map2 extends JFrame implements Runnable {
         //scale height to maintain aspect ratio
         new_height = (new_width * original_height) / original_width;
     }
-
     // then check if we need to scale even with the new height
     if (new_height > bound_height) {
         //scale height to fit instead
@@ -115,11 +79,9 @@ public class Map2 extends JFrame implements Runnable {
         //scale width to maintain aspect ratio
         new_width = (new_height * original_width) / original_height;
     }
-
     return new Dimension(new_width, new_height);
 }
 	
-
 	public static BufferedImage resize(BufferedImage image, Dimension requiredImage) 
 	{
 		int height = (int) requiredImage.getHeight();
